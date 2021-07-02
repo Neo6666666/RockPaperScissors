c336b0db52d4c8acda8f5469290dbe8c63dff8a7
neo6666666 <neo6666666@gmail.com>
2021-07-02 22:52:08 +0700

    Init commit

---
+++ backend/src/main.py
@@ -0,0 +1,106 @@
+from typing import List
+
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.responses import HTMLResponse, JSONResponse
+
+from tortoise.contrib.fastapi import register_tortoise
+
+from .users.models import Status, User_Pydantic, UserIn_Pydantic, Users
+
+
+app = FastAPI()
+
+
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chat</title>
+    </head>
+    <body>
+        <h1>WebSocket Chat</h1>
+        <h2>Your ID: <span id="ws-id"></span></h2>
+        <form action="" onsubmit="sendMessage(event)">
+            <input type="text" id="messageText" autocomplete="off"/>
+            <button>Send</button>
+        </form>
+        <ul id='messages'>
+        </ul>
+        <script>
+            var client_id = Date.now()
+            document.querySelector("#ws-id").textContent = client_id;
+            var ws = new WebSocket(`ws://localhost:8000/ws/${client_id}`);
+            ws.onmessage = function(event) {
+                var messages = document.getElementById('messages')
+                var message = document.createElement('li')
+                var content = document.createTextNode(event.data)
+                message.appendChild(content)
+                messages.appendChild(message)
+            };
+            function sendMessage(event) {
+                var input = document.getElementById("messageText")
+                ws.send(input.value)
+                input.value = ''
+                event.preventDefault()
+            }
+        </script>
+    </body>
+</html>
+"""
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        message = await User_Pydantic.from_queryset(Users.exclude(status=Status.OFFLINE))
+        print(message)
+        await self.send_personal_message(message=f"'users': {[m.json(indent=4) for m in message]}", websocket=websocket)
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_json(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+
+manager = ConnectionManager()
+
+
+@app.get("/")
+async def get():
+    return HTMLResponse(html)
+
+
+@app.get("/users", response_model=List[User_Pydantic])
+async def get_users():
+    return await User_Pydantic.from_queryset(Users.exclude(status=Status.OFFLINE))
+
+
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.send_personal_message(f"You wrote: {data}", websocket)
+            await manager.broadcast(f"Client #{client_id} says: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Client #{client_id} left the chat")
+
+
+register_tortoise(
+    app,
+    db_url="sqlite://database.db",
+    modules={"models": ["src.users.models"]},
+    generate_schemas=True,
+    add_exception_handlers=True,
+)
---
+++ backend/src/users/models.py
@@ -0,0 +1,25 @@
+from enum import Enum
+
+from tortoise import fields, models
+from tortoise.contrib.pydantic import pydantic_model_creator
+
+
+class Status(str, Enum):
+    ACTIVE = 'Active'
+    IN_GAME = 'In Game'
+    OFFLINE = 'Offline'
+
+
+class Users(models.Model):
+    id = fields.IntField(pk=True)
+    username = fields.CharField(max_length=20, unique=True)
+    password_hash = fields.CharField(max_length=128, null=True)
+    status = fields.CharEnumField(Status)
+
+    class PydanticMeta:
+        exclude = ["password_hash"]
+
+
+User_Pydantic = pydantic_model_creator(Users, name="User")
+UserIn_Pydantic = pydantic_model_creator(
+    Users, name="UserIn", exclude_readonly=True)
